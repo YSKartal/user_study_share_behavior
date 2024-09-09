53da3d080038664fb40c8d90498dd5d2fb22c964
YSKartal <yskartal43@gmail.com>
2024-09-09 22:35:40 +0200

    new posts

--- my-react-node-app/my-react-app/src/PostEval.js
+++ my-react-node-app/my-react-app/src/PostEval.js
@@ -11,24 +11,45 @@ import { Button } from 'bootstrap';
 
 function ContentView() {
 
-    var sdt = {
-        posts: [
-            {
-                share: 0
-            },
-            {
-                share: 0
-            },
-            {
-                share: 0
-            },
-            {
-                share: 0
-            }
-        ]
-    };
+    const lPostTzpes = [3, 3, 3, 3, 1, 1, 1, 1, 2, 2, 2, 2, 0, 0, 0, 0]
     const [shopCart, setShopCart] = useState({
         posts: [
+            {
+                share: 0
+            },
+            {
+                share: 0
+            },
+            {
+                share: 0
+            },
+            {
+                share: 0
+            },
+            {
+                share: 0
+            },
+            {
+                share: 0
+            },
+            {
+                share: 0
+            },
+            {
+                share: 0
+            },
+            {
+                share: 0
+            },
+            {
+                share: 0
+            },
+            {
+                share: 0
+            },
+            {
+                share: 0
+            },
             {
                 share: 0
             },
@@ -52,7 +73,6 @@ function ContentView() {
 
     let idx = 0;
     let content = mdata.posts[idx];
-    let content_2 = mdata.posts[1];
 
     function ButtonShare(btnIdx) {
         const [buttonText, setButtonText] = useState(' Share');
@@ -71,9 +91,10 @@ function ContentView() {
             console.log(btnIdx);
         }
 
-        return (<button type="button" class="btn btn-primary  btn-lg share-1 d-flex justify-content-center position-relative end-0" style={{ width: "100%" }} onClick={handleClick}>
-            <p style={{ height: "300", marginBottom: "0", fontSize: "30px" }}>
-                <svg style={{ height: "40px", width: "40px", fill: "white" }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M272 416c17.7 0 32-14.3 32-32s-14.3-32-32-32l-112 0c-17.7 0-32-14.3-32-32l0-128 32 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-64-64c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l32 0 0 128c0 53 43 96 96 96l112 0zM304 96c-17.7 0-32 14.3-32 32s14.3 32 32 32l112 0c17.7 0 32 14.3 32 32l0 128-32 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8l-32 0 0-128c0-53-43-96-96-96L304 96z" /></svg>
+
+        return (<button type="button" data-bs-toggle="button" class="btn btn-outline-primary  btn-lg share-1 d-flex justify-content-center position-relative end-0" style={{ width: "35%" }} onClick={handleClick}>
+            <p style={{ height: "300", marginBottom: "0", fontSize: "23px" }}>
+                <svg style={{ height: "30px", width: "30px", fill: "blue" }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M272 416c17.7 0 32-14.3 32-32s-14.3-32-32-32l-112 0c-17.7 0-32-14.3-32-32l0-128 32 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-64-64c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l32 0 0 128c0 53 43 96 96 96l112 0zM304 96c-17.7 0-32 14.3-32 32s14.3 32 32 32l112 0c17.7 0 32 14.3 32 32l0 128-32 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8l-32 0 0-128c0-53-43-96-96-96L304 96z" /></svg>
                 {buttonText}
             </p>
         </button>);
@@ -81,12 +102,32 @@ function ContentView() {
 
 
 
+    function RatingText(value) {
+        if (value > 3) {
+            return (<p>Please briefly describe why you trust this post?</p>)
+        }
+        else if (value < 3) {
+            return (<p>Please briefly describe why you distrust this post?</p>)
+        }
+        else {
+            return (<p>Please briefly explain why you neither trust nor distrust this article?</p>)
+        }
+    }
+
     function RatingTrust() {
 
+        const [rateValue, setRateValue] = useState(3);
+        const setRV1 = () => setRateValue(1);
+        const setRV2 = () => setRateValue(2);
+        const setRV3 = () => setRateValue(3);
+        const setRV4 = () => setRateValue(4);
+        const setRV5 = () => setRateValue(5);
+
+
         if (visibleRT) {
-            return (<div className="d-flex justify-content-center" style={{ width: "100%" }}>
+            return (<div className="d-flex justify-content-center" style={{ width: "100%", marginLeft: "5%", marginTop: "5%", fontSize: "18px"}}>
                 <div>
-                    <p>How trustworthy do you find this post with additional information?</p>
+                    <p>How trustworthy do you find this post on a scale of 1 (lowest) to 5 (highest)?</p>
                     <Form>
                         {['radio'].map((type) => (
                             <div key={`inline-${type}`} className="mb-3">
@@ -95,6 +136,7 @@ function ContentView() {
                                     label="1"
                                     name="group1"
                                     type={type}
+                                    onChange={setRV1}
                                     id={`inline-${type}-1`}
                                 />
                                 <Form.Check
@@ -102,6 +144,7 @@ function ContentView() {
                                     label="2"
                                     name="group1"
                                     type={type}
+                                    onChange={setRV2}
                                     id={`inline-${type}-2`}
                                 />
                                 <Form.Check
@@ -109,6 +152,7 @@ function ContentView() {
                                     label="3"
                                     name="group1"
                                     type={type}
+                                    onChange={setRV3}
                                     id={`inline-${type}-3`}
                                 />
                                 <Form.Check
@@ -116,6 +160,7 @@ function ContentView() {
                                     label="4"
                                     name="group1"
                                     type={type}
+                                    onChange={setRV4}
                                     id={`inline-${type}-4`}
                                 />
                                 <Form.Check
@@ -123,11 +168,17 @@ function ContentView() {
                                     label="5"
                                     name="group1"
                                     type={type}
+                                    onChange={setRV5}
                                     id={`inline-${type}-5`}
                                 />
                             </div>
                         ))}
                     </Form>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>{RatingText(rateValue)}</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+
                 </div>
             </div>
             );
@@ -135,8 +186,8 @@ function ContentView() {
         else {
             return (
                 <div>
-                    
-            </div>
+
+                </div>
             );
         }
     }
@@ -186,7 +237,7 @@ function ContentView() {
     function AccordionPMention(data) {
         return (<Accordion alwaysOpen>
             <Accordion.Item eventKey="1">
-                <Accordion.Header>Post Content Mention in the Publication</Accordion.Header>
+                <Accordion.Header>Short Evidence in the Publication</Accordion.Header>
                 <Accordion.Body>
                     <p>  {data.mentionInPaper} </p>
                 </Accordion.Body>
@@ -197,7 +248,7 @@ function ContentView() {
     function AccordionSummary(data) {
         return (<Accordion alwaysOpen>
             <Accordion.Item eventKey="2">
-                <Accordion.Header>Summary of the Publication</Accordion.Header>
+                <Accordion.Header>Short AI-Generated Summary of the Publication</Accordion.Header>
                 <Accordion.Body>
                     <p> {data.extSummaryMention} </p>
                 </Accordion.Body>
@@ -231,7 +282,7 @@ function ContentView() {
                 <div style={{ marginLeft: "1%" }}>
                     <p><i class="bi bi-person-circle" style={{ height: "40px", width: "40px", fontSize: 40 }}></i> </p>
                 </div>
-                <div className="rounded-3 d-grid gap-3" style={{ paddingLeft: "1%", width: "150%" }}>
+                <div className="rounded-3 d-grid gap-3" style={{ paddingLeft: "1%", width: "150%", fontSize:"18px" }}>
                     <p> <b>Max @max</b></p>
                     <p>  {data.mainPost} <a href={data.mainURL}>{data.mainRURL}</a></p>
                     <div>
@@ -254,19 +305,21 @@ function ContentView() {
     return (<div className='ml-1' style={{ marginLeft: "20%", marginRight: "20%", width: "60%", marginTop: "3%", fontSize: 20 }}>
         <h1>Timeline</h1>
         <div className="rounded-3 " style={{ marginTop: "2%", marginBottom: "2%" }}>
-            {SetPost(content, 0, 0)}
-            {SetPost(content, 1, 1)}
-            {SetPost(content, 2, 2)}
-            {SetPost(content, 3, 3)}
+            <div>
+                {mdata.posts.map((item, index) => (
+                    SetPost(item, lPostTzpes[index], index)
+                ))}
+            </div>
+
         </div>
-        <div style={{ marginBottom: "3%"}}>
-            <button type="button" class="btn btn-outline-primary  btn-lg share-1 d-flex justify-content-center position-relative end-0" style={{ width: "100%" }} onClick={makeVisible}>
-            <p style={{ height: "300", marginBottom: "0", fontSize: "30px" }}>
+        <div style={{ marginBottom: "3%" }}>
+            <button type="button" class="btn btn-outline-danger  btn-lg share-1 d-flex justify-content-center position-relative end-0" style={{ width: "100%" }} onClick={makeVisible}>
+                <p style={{ height: "300", marginBottom: "0", fontSize: "25px" }}>
                 Next Page >
-            </p>
-        </button>
+                </p>
+            </button>
         </div>
-        
+
     </div>)
 
 }
